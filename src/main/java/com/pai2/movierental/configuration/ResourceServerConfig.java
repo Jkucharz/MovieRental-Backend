e7ce63eb13a6279308657403ed03d7645e03a559
KamilKwiecien <kmail.kwiecien@gmail.com>
2019-05-30 00:04:36 +0200

    Konfiguracja CORS'a

--- src/main/java/com/pai2/movierental/configuration/ResourceServerConfig.java
+++ src/main/java/com/pai2/movierental/configuration/ResourceServerConfig.java
@@ -1,21 +1,32 @@
 package com.pai2.movierental.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.web.session.SessionManagementFilter;
 
 @Configuration
 @EnableResourceServer
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+    @Bean
+    CorsFilter corsFilter() {
+        CorsFilter filter = new CorsFilter();
+        return filter;
+    }
+
     @Override
     public void configure(HttpSecurity http) throws Exception {
         http
+                .addFilterBefore(corsFilter(), SessionManagementFilter.class)
                 .headers()
                 .frameOptions()
                 .disable()
                 .and()
                 .authorizeRequests()
+                .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                 .antMatchers("/").permitAll()
                 .antMatchers("/admin/**").hasAuthority("ROLE_ADMIN");
     }
